6c27e95520f9df0ecb5ecba153cd60cb59e7d950
Vikram Rawat <vickey.coool@gmail.com>
2026-03-06 12:36:47 +0530

    feat: update hotwidget to convert R data frame to 2D array for Handsontable initialization

--- inst/htmlwidgets/hotwidget.js
+++ inst/htmlwidgets/hotwidget.js
@@ -12,9 +12,24 @@ HTMLWidgets.widget({
         container.id = 'handsontable-' + Math.floor(Math.random() * 1000000);
         el.appendChild(container);
 
+        // Convert R data frame (column-oriented) to 2D array (row-oriented)
+        let tableData = [];
+        if (x.data && typeof x.data === 'object') {
+          const columns = Object.keys(x.data);
+          const numRows = columns.length > 0 ? x.data[columns[0]].length : 0;
+
+          for (let i = 0; i < numRows; i++) {
+            let row = [];
+            for (let col of columns) {
+              row.push(x.data[col][i]);
+            }
+            tableData.push(row);
+          }
+        }
+
         // Initialize Handsontable
         const hot = new Handsontable(container, {
-          data: x.data,
+          data: tableData,
           rowHeaders: true,
           colHeaders: x.colHeaders || true,
           height: 'auto',
