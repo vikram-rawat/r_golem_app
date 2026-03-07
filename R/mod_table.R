fed410b0d3ff714adc2126bde5444248b191c9ee
Vikram Rawat <vickey.coool@gmail.com>
2026-03-07 11:09:03 +0530

    feat: update mod_table_ui layout for improved button alignment and spacing

--- R/mod_table.R
+++ R/mod_table.R
@@ -11,14 +11,13 @@ mod_table_ui <- function(id) {
         width = 1,
         h1("MTCars DataSet"),
         h6("interactive data table with real-time editing"),
-        layout_columns(
-          width = c(-2, 12, -2),
+        div(
+          class = "d-flex justify-content-between",
           actionButton(
             inputId = ns("save_btn"),
             label = "Save Changes",
             icon = icon("save")
           ),
-          div(""),
           actionButton(
             inputId = ns("reset_btn"),
             label = "Reset Changes",
